e2158b1379bd7817ec0ef7cdf49a9ea91b0ec83a
Sebastian Paiva <austral@gamerpvp.com>
2014-07-14 01:13:12 -0400

    Fix

--- lib/lol/league_request.rb
+++ lib/lol/league_request.rb
@@ -22,7 +22,11 @@ module Lol
     # @return [Array]
     # TODO: Change name to entries?
     def get_entries summoner_id
-      perform_request(api_url("league/by-summoner/#{summoner_id}/entry")).map { |e| League.new e.first}
+      returns
+      perform_request(api_url("league/by-summoner/#{summoner_ids.join(",")}")).each do |s, l|
+        returns = l.map {|data| League.new data}
+      end
+      returns
     end
 
     # Retrieves leagues data for team
